661a7b0da46ebc4fa42448a07f0b1e20eaccf42d
faezehabdolazimi <75137625+faezehabdolazimi@users.noreply.github.com>
2024-11-08 18:05:47 +0330

    Update and rename switch-case2 to name-switch.java

---
+++ name-switch.java
@@ -0,0 +1,16 @@
+package ir.isc.training;
+
+public class name {
+      static String names= "ali";
+      
+	public static void main(String[] args) {
+		switch (names) {
+		case "ali","javad" -> System.out.println("welcome man"); 
+		case "arefeh","zahra" -> System.out.println("welcome womanman");
+		
+		default -> System.out.println("who are you?"); 
+		
+		}
+	}
+
+}
